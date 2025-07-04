89dabbcaaf37a474df99c451bf107b9fb0ab611e
Agnick <agnickghosh15@gmail.com>
2025-07-04 07:34:13 +0530

    Create Question18.c

---
+++ Pattern_Printing/Question18.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int i,j,s;  //space = s
+    for(i=0; i<=5;i++){
+        
+        for(j=1; j<=i; j++){
+            
+                printf("%c", 64+j);
+            
+            }
+          
+        printf("\n");
+
+        }
+
+     
+
+        return 0;
+
+}
+        
+    
+
+    
+    
+
+/*
+
+A
+AB
+ABC
+ABCD
+ABCDE
+
+*/
